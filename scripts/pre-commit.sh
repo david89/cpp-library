ca127d18cfbe6b128449170483df4e9e9f3d26ed
David Gómez <gomez.david.89@gmail.com>
2020-04-26 19:54:54 -0700

    Small change to the pre-commit hook.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sure we always run from root
-cd "${0%/*}/.."
-
 # Adapted from https://github.com/andrewseidl/githook-clang-format
 format_file() {
   file="${1}"
@@ -16,6 +13,9 @@ echo "Formatting files"
 for file in `git diff-index --cached --name-only HEAD` ; do
   format_file "${file}"
 done
+
+# Make sure we always run from root
+cd "${0%/*}/.."
 
 echo "Running tests"
 bazel test --cxxopt='-std=c++11' //string_view:string_view_test
