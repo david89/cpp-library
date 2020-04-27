1eb1f8c9ce7afa2abec79922b4c0bd7ea600b400
David Gómez <gomez.david.89@gmail.com>
2020-04-26 19:24:16 -0700

    Adding support for automatically formatting files at pre-commit time.
    Hook heavily based on
    https://github.com/andrewseidl/githook-clang-format

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,6 +3,20 @@
 # Make sure we always run from root
 cd "${0%/*}/.."
 
+# Adapted from https://github.com/andrewseidl/githook-clang-format
+format_file() {
+  file="${1}"
+  if [ -f $file ]; then
+    clang-format -i ${1}
+    git add ${1}
+  fi
+}
+
+echo "Formatting files"
+for file in `git diff-index --cached --name-only HEAD` ; do
+  format_file "${file}"
+done
+
 echo "Running tests"
 bazel test --cxxopt='-std=c++11' //string_view:string_view_test
 if [ $? -ne 0 ]; then
