7cc45c51b9cbdc105dd9f83c2b83b74b31960221
David Gómez <gomez.david.89@gmail.com>
2020-05-17 16:24:43 -0700

    Add support for copy constructors.
    There are some overloads depending on if the type is copy constructible
    and trivially copy constructible.
    
    Tested:
    bazel test //types:optional_test

--- types/optional.h
+++ types/optional.h
@@ -2,17 +2,62 @@
 #define TYPES_OPTIONAL
 
 #include <exception>
+#include <iostream>
 #include <memory>
 
 namespace david {
 namespace internal {
 
 template <typename T>
-struct optional_rep {
-  bool engaged = false;
+class optional_rep {
+ public:
+  optional_rep() = default;
+
+ protected:
+  bool engaged_ = false;
   // TODO: this doesn't work for non-trivially default constructable objects.
   // Let's use an union here and some traits magic here.
-  T obj;
+  T obj_;
+};
+
+template <typename T,
+          bool copy_constructible = std::is_copy_constructible<T>::value,
+          bool trivially_copy_constructible =
+              std::is_trivially_copy_constructible<T>::value>
+class optional_constructors;
+
+// Trivially copy constructible types shouldn't have a copy constructor,
+// otherwise it's no longer a trivial constructor.
+template <typename T>
+class optional_constructors<T, true, true> : public optional_rep<T> {
+ public:
+};
+
+template <typename T>
+class optional_constructors<T, true, false> : public optional_rep<T> {
+ public:
+  optional_constructors() noexcept = default;
+  optional_constructors(const optional_constructors& other) {
+    this->engaged_ = other.engaged_;
+    if (other.engaged_) {
+      this->obj_ = other.obj_;
+    }
+  }
+};
+
+// Non copy constructible types cannot be instantiated.
+template <typename T>
+class optional_constructors<T, false, true> : public optional_rep<T> {
+ public:
+  optional_constructors() noexcept = default;
+  optional_constructors(const optional_constructors&) = delete;
+};
+
+template <typename T>
+class optional_constructors<T, false, false> : public optional_rep<T> {
+ public:
+  optional_constructors() noexcept = default;
+  optional_constructors(const optional_constructors&) = delete;
 };
 
 }  // namespace internal
@@ -25,19 +70,17 @@ struct nullopt_t {
 // units?
 static const nullopt_t nullopt{0};
 
+// Interface from https://en.cppreference.com/w/cpp/utility/optional
 template <typename T>
-class optional {
+class optional : public internal::optional_constructors<T> {
  public:
   // Constructors.
-  optional() = default;
-  optional(nullopt_t){};
+  optional() noexcept = default;
+  optional(nullopt_t) noexcept {};
 
   // Observers.
-  explicit operator bool() const noexcept { return rep_.engaged; }
-  bool has_value() const noexcept { return rep_.engaged; }
-
- private:
-  internal::optional_rep<T> rep_;
+  explicit operator bool() const noexcept { return this->engaged_; }
+  bool has_value() const noexcept { return this->engaged_; }
 };
 }  // namespace david
 
--- types/optional_test.cc
+++ types/optional_test.cc
@@ -1,22 +1,12 @@
 #include "types/optional.h"
 
+#include <string>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace david {
 namespace {
-
-using ::testing::Eq;
-using ::testing::Ge;
-using ::testing::Gt;
-using ::testing::HasSubstr;
-using ::testing::IsEmpty;
-using ::testing::IsNull;
-using ::testing::Le;
-using ::testing::Lt;
-using ::testing::Ne;
-using ::testing::Not;
-using ::testing::SizeIs;
 
 TEST(Optional, DefaultConstructor) {
   optional<int> s;
@@ -28,6 +18,30 @@ TEST(Optional, NulloptConstructor) {
   optional<int> s(nullopt);
   EXPECT_FALSE(s.has_value());
   EXPECT_FALSE(bool(s));
+}
+
+TEST(Optional, IsTriviallyCopyConstructible) {
+  optional<int> a;
+  optional<int> b(a);
+  EXPECT_FALSE(b.has_value());
+  EXPECT_FALSE(bool(b));
+}
+
+TEST(Optional, IsNonTriviallyCopyConstructible) {
+  optional<std::string> a;
+  optional<std::string> b(a);
+  EXPECT_FALSE(b.has_value());
+  EXPECT_FALSE(bool(b));
+}
+
+TEST(Optional, IsNonCopyConstructible) {
+  struct A {
+    A() = default;
+    A(A&&) {}
+  };
+  optional<A> a;
+  // This line doesn't compile as expected.
+  // optional<A> b(a);
 }
 
 }  // namespace
